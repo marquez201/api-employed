247d8caad53520d59b1e888b3a74511422848dca
Ricardo Marquez <r.marquez18@info.uas.edu.mx>
2024-06-21 10:09:38 -0700

    Description: Se agregan Logs. Developer: Ricardo Marquez.

--- empleados/src/main/java/com/rmv/empleados/repository/EmployedRepository.java
+++ empleados/src/main/java/com/rmv/empleados/repository/EmployedRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rmv.empleados.entitys.EmployedEntity;
 
+@Repository
 public interface EmployedRepository extends MongoRepository<EmployedEntity, String> {
     
     List<EmployedEntity> findByFirstName(String firstName);
